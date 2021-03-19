92c2e423ade6fce933928720328f8c9e63630e4d
amarjeetprasad <56919207+amarjeetprasad@users.noreply.github.com>
2021-03-19 18:45:57 +0530

    Updates

---
+++ precache-manifest.e57a3b49e38a25885c6db0c448ff0936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8eda6399adb8d552f80380c8f771630",
+    "url": "/our-reviews/index.html"
+  },
+  {
+    "revision": "b33fc9d4a9efa2c18d22",
+    "url": "/our-reviews/static/css/main.e64ab053.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/our-reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/our-reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33fc9d4a9efa2c18d22",
+    "url": "/our-reviews/static/js/main.37db3349.chunk.js"
+  },
+  {
+    "revision": "aac6eb915979c83e89b3",
+    "url": "/our-reviews/static/js/runtime-main.f6559b56.js"
+  }
+]);
